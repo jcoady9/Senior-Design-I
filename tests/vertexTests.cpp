1a42fea73d06899e79a2a6299acb98440cc4cdaa
jcoady9 <jcoady@ycp.edu>
2014-10-07 19:57:46 -0400

    Made minor adjustments to Line class header and did some work with the test cases for the classes.

--- tests/vertexTests.cpp
+++ tests/vertexTests.cpp
@@ -13,11 +13,15 @@ int main(){
 	Vertex* v1 = new Vertex(arr[0] , arr[1]);
 	Vertex* v2 = new Vertex(arr[2] , arr[3]);
 	Vertex* v3 = new Vertex(arr[4], arr[5]);
-
-	printf("Testing Vertex class........\n\n");
-
-	printf("Testing getPoint method...\n\n");
+	printf("-------------------------------------------\n");
+	printf("Testing Vertex class........\n");
+	printf("-------------------------------------------\n\n");
 	
+	printf("-------------------------------------------\n");
+	printf("Testing getPoint() method...\n");
+	printf("-------------------------------------------\n\n");
+	
+	//the getPoint method is being tested by comparing the x, y-coordinates with the array of int values declared above. 
 	int point[2];	
 
 	v1->getPoint(point);
@@ -29,9 +33,12 @@ int main(){
 	v3->getPoint(point);
 	printf("Correct output is -> x: %i, y: %i\nv3 output is -> x: %i, y: %i\n\n", 100, 350, point[0], point[1]);
 
+	printf("-------------------------------------------\n");
+	printf("Testing addLine() & getLine() methods...\n");
+	printf("-------------------------------------------\n\n");
 
-	printf("Testing addLine method...\n\n");	
-
+	//We are testing addLine() and getLine() by comparing the address' of the Line object at initialization and
+	//the address' of the Line pointer returned by the getLine() method
 	Line* l1 = new Line(v1);
 	Line* l2 = new Line(v3);
 	Line* l3 = new Line(v2);
@@ -40,29 +47,34 @@ int main(){
 	v1->addLine(l2);
 	v1->addLine(l3);
 
-	printf("lines added to vertex\n\n");
+	printf("line[0] addr: %p\n", v1->getLine(0)->getVertex());
+	printf("line[1] addr: %p\n", v1->getLine(1));
+	printf("line[2] addr: %p\n\n", v1->getLine(2));
 	
+
+	printf("		v1 addr: %p\n", v1);
+	Vertex* tmp = v1->getLine(0)->getVertex();
+	Line* line = v1->getLine(0);
+	printf("line[0]->next_vertex addr: %p\n\n", line->getVertex());
+	
+	printf("		v3 addr: %p\n", v3);
+	tmp = v1->getLine(1)->getVertex();
+	printf("line[1]->next_vertex addr: %p\n\n", tmp);
+	
+	
+	printf("		v2 addr: %p\n", v2);
+	tmp = v1->getLine(2)->getVertex();
+	printf("line[2]->next_vertex addr: %p\n\n", tmp);	
+	
+
 	printf("l1 addr: %p\n", l1);
 	printf("l2 addr: %p\n", l2);
 	printf("l3 addr: %p\n\n", l3);
-
-	Line* tmp = v1->getLine(0);
 	
-	printf("v1->line[0] addr: %p\n\n", tmp);
+	printf("-------------------------------------------\n");
+	printf("End of Tests...\n");
+	printf("-------------------------------------------\n");
 
-	tmp = v1->getLine(1);
-	printf("v1->line[1] addr: %p\n\n", tmp);
-
-	tmp = v1->getLine(2);
-	printf("v1->line[2] addr: %p\n\n", tmp);
-
-	printf("v1 addr: %p\n", v1);
-	printf("v2 addr: %p\n", v2);
-	printf("v3 addr: %p\n\n", v3);
-	
-	
-
-	printf("%p\n\n", v1->getLine(0)->getVertex());
 
 	return 0;
 }
