26f38e4618549d866f0d91258ec4807943b193fa
Sam <shamor.sh@gmail.com>
2014-10-19 18:36:12 -0400

    Robot arm moves, With assistance of Shane.

--- motor_movement/motor_movement.ino
+++ motor_movement/motor_movement.ino
@@ -1,6 +1,5 @@
 #include <BioloidController.h>
 #include <ax12.h>
-
 #include <Motors2.h>
 
 // we always need to create an instance of the bioloid control, usually with baud = 1Mbps.
@@ -8,36 +7,50 @@ BioloidController bioloid = BioloidController(1000000);
 
 int i;
 void setup(){
-    i = 0;
+    i = 512;
+    Serial.begin(38400); //start serial communications at 38400bps
 }
 
 void loop(){
-    // set AX-12 servo with ID=1, to position i, where 0=<i<1024
-    SetPosition(3,i);
-    i = (i++)%1024;
+    // set AX-12 servo with ID=1, to position i, where 0=<i<950 because of extention
+   int inByte = Serial.read();
+   Serial.println("Running...");
+   while(i<700){
+     delay(50);
+      SetPosition(2,i);
+     SetPosition(1,i);
+     i++;
+     
+     int inByte = Serial.read();       
+      switch (inByte)
+      {
+        case '0':    
+          relaxArm();
+          exit(0);
+        break;    
+      }
+   }
+   
+   while(i>512){
+     switch (inByte)
+      {
+        case '0':    
+          relaxArm();
+          exit(0);
+        break;    
+      }
+      
+     delay(50);
+     SetPosition(2,i);
+     SetPosition(1,i);
+     i--;
+   }
+   
 }
 
-
-/*Motors2 drive = Motors2();
-
-void setup() {
-    drive.set(-255,-255); // drive backwards at full speed 
-    delay(1000);
-
-    drive.set(0,0); // stop
-    delay(1000);
-
-    drive.set(100,100); // drive forward at half speed
-    delay(1000);
-
-    drive.left(0); // stop the left motor, turn in place
-    delay(500);    
-    drive.right(50); // cut right motor speed in half
-    delay(500);
-    drive.right(0); // and stop
+void relaxArm(){
+    i=512; 
+    SetPosition(2,i);
+    SetPosition(1,i);
 }
 
-void loop(){
-    
-}
-*/
