3bed0eb46c521d0a4170ae762f0daa8cdaa4ecb5
J-Fid <jfiddle@ycp.edu>
2014-10-23 17:29:53 -0400

    Started working on converting a vector to verticies to run through DFS.

---
+++ src/vec2vertex.cpp
@@ -0,0 +1,10 @@
+//Convert Vec4i into vertexes.  Run vertexes through DFS.
+
+#include "../include/dfs.h"
+#include "../include/vertex.h"
+
+void Vec2Vertex(vector<Vec4i> &vec)
+{
+	Vertex v1 = new Vertex(vec[0], vec[1]);
+	Vertex v2 = new Vertex(vec[2], vec[3]);
+}
