e1b27f2f97e91aee1ddc786bad43ae601ae45b0f
Shane <sbonner1@ycp.edu>
2014-11-02 18:24:30 -0500

    Problems with my OpenCV, committing line simulation classes for Sam to test on her machine. Moved my classes around. Created drawLine header file and class, along with the testDrawLine class.

--- src/drawLineTest.cpp
+++ src/drawLineTest.cpp
@@ -1,34 +1,21 @@
 #include<stdio.h>
 #include<errno.h>
 
+#include "../include/drawLine.h"
 
 using namespace std;
 const char * filename = "testOutput.txt";
+int drawLine();
 
-int drawLine() {
-	FILE *file;
-	file = fopen(filename,"w"); 
+int main(){
+	printf("-----------------------------------------------\n");
+	printf("\Testing the drawLine simulation...\n");
+	printf("-----------------------------------------------\n");
 
-	if(!file){
-		cout << "Test file does not exist" << "\n";
-		exit(1);
-	}
+	//Run it
+	drawLine();
+
 	
-	string coordinates;
-	char data[128];
-
-	ifstream filestream;
-	filestream.open(filename);
-	
-	data[0]=0; // read in each line of the file until we're done 
-	do{ 
-		if(!isspace(data[0]) && data[0] != 0) {
-		    lastLine = data; 
-		}
-		 
-        }while (filestream.getline(data, 256)); 
-
-	//End of file reached, close stream
-	filestream.close(); 
 
 }
+
