87c486706c9edef9b4e4cf15000fe382092ec1c8
Shane <sbonner1@ycp.edu>
2014-11-02 18:34:21 -0500

    once again amending previous commits. Files were in the incorrect locations.

--- src/drawLine.cpp
+++ src/drawLine.cpp
@@ -1,11 +1,15 @@
-#include<stdio.h>
-#include<errno.h>
+#include <stdio.h>
+#include <errno.h>
+#include <vector>
+#include <opencv2/highgui/highgui.hpp>
 
+using namespace std;
 
-int main(){
+int drawPic(){
 
 	FILE *file;
 	file = fopen(filename,"w"); 
+	Mat img;
 
 	if(!file){
 		cout << "Test file does not exist" << "\n";
@@ -30,6 +34,33 @@ int main(){
 	filestream.close(); 
 	
 	cout << "data = " << lastLine << "\n";
+
+	std::vector<int> vect;
+
+	std::stringstream ss(str);
+
+	int i;
+
+	while (ss >> i)
+	{
+	    vect.push_back(i);
+
+	    if (ss.peek() == ',')
+		ss.ignore();
+	}
 	
+	//create a black image
+	img = np.zeros((512,512,3), np.uint8);
+
+	//draw a diagonal blue line with the parsed coordinates
+	cv2.line(img,(0,0),(vec[0],vect[1]),(vect[2],vect[3]),5);
+
+	
+	//alternative attempt at drawing the line
+	//IplImage* imageScribble = cvCreateImage(cvSize(320,240),8,3);
+	//cvZero(imgScribble);
+	//cvSet(imgScribble, cvScalar(0,0,0); *****Used to set any color*****
+	//cv2.line(img,(0,0),(vec[0],vect[1]),(vect[2],vect[3]),5);
+
 	
 }	
