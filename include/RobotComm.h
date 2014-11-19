baa3319ba933c5570807b60758b8e027241cdb42
Sam <shamor.sh@gmail.com>
2014-11-18 20:15:58 -0500

    Fixed the draw function by adding it to the RobotCommunication functions and converted that into the RobotComm class.

--- include/RobotComm.h
+++ include/RobotComm.h
@@ -2,27 +2,37 @@
 #define _ROBOTCOMM_H__
 
 #include <stdio.h>
+#include "../include/vertex.h"
 
-/*
-**Method prototype for sending coordinates to the robocontroller
-**x1 and y1 are the current vertex's coordinates
-**x2 and y2 are the next vertex's coordinates. 
-returns -1 if an
-*/
-void sendCoordinates(int x1, int y1, int x2, int y2, FILE * file);  
+class RobotComm {
+	private:
+		const char * TfileName;//test
+		const char * RfileName;//real
+		int checkSum;
 
-/*
-**Method prototype for checking if coordinates were recieved properly through a filestream. 
-**returns -1 if coordinates did not match, -2 if robot disconnected, -3 if wrong coordinates
-*/
-int receiveACK();
+	public:
+		RobotComm();//constructor
+		~RobotComm();
 
+		/*
+			**Method prototype for sending coordinates to the robocontroller
+			**x1 and y1 are the current vertex's coordinates
+			**x2 and y2 are the next vertex's coordinates. 
+		*/
+		void sendCoordinates(int x1, int y1, int x2, int y2, FILE * file);  
 
-/*
-**Method prototype for checking if coordinates were recieved properly through a serialStream. 
-**returns -1 if coordinates did not match, -2 if robot disconnected, -3 if wrong coordinates
-*/
-int receiveACKSerial(FILE * file);	
+		/*
+			**Method prototype for checking if coordinates were recieved properly through a serialStream. 
+			**returns -1 if coordinates did not match, -2 if robot disconnected, -3 if wrong coordinates
+		*/
+		int receiveACKSerial(FILE * file);
+	
+		/*
+			**Accepts first vertex and accesses its other vertex via the nextVertex() functionality
+			**Send the coordinates to the robot. 
+			**Wait for the acknowledgement to come back. 
+		*/
+		void RobotCommunication(Vertex* v);
 
-
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<errno.h>
-
+#include "../include/drawImageSimulator.h"
+#include "../include/drawImageInterface.h"
 #include <opencv2/highgui/highgui.hpp>
-
 #include "../include/vertexSearch.h"
-
 #include "../include/vertex.h"
 #include "../include/vec2vertex.h"
 #include "../include/scale.h"
-#include "../include/draw.h"
+#include "../include/RobotComm.h"
+
 
 
 using namespace cv;
@@ -33,7 +33,7 @@ int main(int argc, char** argv){
 	    printf("image is empty.\n\n");
 	    exit(1);
 	}
-	
+	Mat img2 = imread("black.png", CV_LOAD_IMAGE_COLOR);
 	//open a window to display the image and enter any key to close the window
 	imshow("Source Image", img);
 
@@ -47,13 +47,24 @@ int main(int argc, char** argv){
 	for( size_t i = 0; i < lines.size(); i++ ){
 		cv::Vec4i l = lines[i];	
 		printf("line[%i]: (%i, %i) -> (%i, %i)\n", (int) i, l[0], l[1], l[2], l[3]);
-		//line( bw, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 1, CV_AA);
+		
 		Vertex * temp = vec2Vertex(l);
 		temp = scale(temp, imgSize.width, imgSize.height);
-		Draw(temp, mode); 
+		
+		if(mode == 1){//simulated
+			drawImageSimulator sim;	
+			sim.drawPic(temp);
+		}else if(mode == 2){//actual robot
+			RobotComm Robot;
+			Robot.RobotCommunication(temp);
+		} 
 	}
 
 	waitKey(0);
+	bool wri= cv::imwrite("black.png", img2);
+	if(wri > 0){
+
+	}else
 
 	return 0;
 }
